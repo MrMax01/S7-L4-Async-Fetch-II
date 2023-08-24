b802eee49a7af093e0e725e2b9ff6ef142f3b277
MrMax01 <massimo.mauro00@gmail.com>
2023-08-24 16:41:17 +0200

    load secondary image interactive

--- data.js
+++ data.js
@@ -1,21 +1,12 @@
 const loadImageOne = document.getElementById("load-one");
 const loadImageTwo = document.getElementById("load-two");
 
-loadImageOne.addEventListener("click", () => {
-  fetch("https://api.pexels.com/v1/search?query=nature", {
-    headers: {
-      Authorization: "lbZxEUsGiESDpuXjg1igT1lnnokBw31urgWZleGRlZsa5tXsNDUteewa",
-    },
-  })
-    .then((resp) => {
-      return resp.json();
-    })
-    .then((naturePhotos) => {
-      const photos = naturePhotos.photos;
-      const containerPhotos = document.getElementById("book");
-      containerPhotos.innerHTML = "";
-      for (let i = 0; i < 9; i++) {
-        containerPhotos.innerHTML += `
+const getImage = (naturePhotos) => {
+  const photos = naturePhotos.photos;
+  const containerPhotos = document.getElementById("book");
+  containerPhotos.innerHTML = "";
+  for (let i = 0; i < 9; i++) {
+    containerPhotos.innerHTML += `
         <div class="col-md-4">
         <div class="card mb-4 shadow-sm">
          <img src="${photos[i].src.large}" style="object-fit: cover" height="200px">
@@ -36,7 +27,34 @@ loadImageOne.addEventListener("click", () => {
         </div>
       </div>
         `;
-      }
+  }
+};
+loadImageOne.addEventListener("click", () => {
+  fetch("https://api.pexels.com/v1/search?query=nature", {
+    headers: {
+      Authorization: "lbZxEUsGiESDpuXjg1igT1lnnokBw31urgWZleGRlZsa5tXsNDUteewa",
+    },
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((naturePhotos) => {
+      getImage(naturePhotos);
+    })
+    .catch((error) => console.log(error));
+});
+
+loadImageTwo.addEventListener("click", () => {
+  fetch("https://api.pexels.com/v1/search?query=Tigers", {
+    headers: {
+      Authorization: "lbZxEUsGiESDpuXjg1igT1lnnokBw31urgWZleGRlZsa5tXsNDUteewa",
+    },
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((naturePhotos) => {
+      getImage(naturePhotos);
     })
     .catch((error) => console.log(error));
 });
